a9bacd50c7afc78277e294abe46d478a21618995
Luis Villeda <luisevilleda@gmail.com>
2016-11-02 16:46:35 -0700

    Implement facebook SDK

--- Sembly/ios/Sembly/AppDelegate.m
+++ Sembly/ios/Sembly/AppDelegate.m
@@ -12,10 +12,20 @@
 #import "RCTBundleURLProvider.h"
 #import "RCTRootView.h"
 
+//FacebookSDK import
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+//
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  //FacebookSDK code
+  [[FBSDKApplicationDelegate sharedInstance] application:application
+                           didFinishLaunchingWithOptions:launchOptions];
+  //
+  
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
@@ -33,5 +43,26 @@
   [self.window makeKeyAndVisible];
   return YES;
 }
+
+//FacebookSDK code
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+  
+  BOOL handled = [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                                openURL:url
+                                                      sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
+                                                             annotation:options[UIApplicationOpenURLOptionsAnnotationKey]
+                  ];
+  // Add any custom logic here.
+  return handled;
+}
+//
+
+//FacebookSDK Log App Activations
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+  [FBSDKAppEvents activateApp];
+}
+//
 
 @end
