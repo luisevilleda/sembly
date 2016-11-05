596c7fc23c5f7c9b60f206f2d11feb0e9578455a
Luis Villeda <luisevilleda@gmail.com>
2016-11-05 13:47:30 -0700

    Update feed to use facebookId

--- Sembly/app/components/Feed.js
+++ Sembly/app/components/Feed.js
@@ -1,78 +1,64 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  Text,
   View,
-  Navigator,
-  TouchableHighlight,
-  TouchableOpacity,
-  ScrollView
+  ScrollView,
 } from 'react-native';
 
 import Spinner from './Spinner';
 
-import Drawer from 'react-native-drawer';
-import NewEventModal from './NewEventModal.js';
-import EventModal from './EventModal.js';
-import NewEventFab from './NewEventFab.js';
+import NewEventModal from './NewEventModal';
+import EventModal from './EventModal';
+import NewEventFab from './NewEventFab';
 
-import TopBar from './TopBar';
 import OurDrawer from './OurDrawer';
-import Menu from './Menu';
 import EventCard from './EventCard';
 
 import _navigate from './navigateConfig';
 
 
 export default class Feed extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       loading: true,
       eventModal: false,
-      addEventModal: false
-    }
+      addEventModal: false,
+    };
   }
   componentWillMount() {
     if (this.props.page === 'bundle') {
-      this.getBundle()
-    }
-    else if (this.props.page === 'invited') {
+      this.getBundle();
+    } else if (this.props.page === 'invited') {
       this.getInvited();
     } else if (this.props.page === 'saved') {
       this.getSaved();
     }
   }
   openEvent(eventId) {
-    this.setState({eventModal: true, eventId: eventId, addEventModal: false});
+    this.setState({ eventModal: true, eventId, addEventModal: false });
   }
-  closeEvent () {
-    this.setState({eventModal:false});
+  closeEvent() {
+    this.setState({ eventModal: false });
   }
-  openModal () {
-    this.setState({addEventModal: true, eventModal:false});
+  openModal() {
+    this.setState({ addEventModal: true, eventModal: false });
   }
   getInvited() {
-    fetch('http://localhost:3000/api/events/invited',{
+    fetch('http://localhost:3000/api/events/invited', {
       method: 'POST',
-      headers: { "Content-Type" : "application/json" },
-      body: JSON.stringify({userId: this.props.user._id})
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ userId: this.props.user.facebookId }),
     })
-    .then(response => {
-      return response.json();
-    })
-    .then( events => {
-      this.setState({events: events, loading: false});
-    })
-    .catch( error => {
-      console.log(error);
-    });
+    .then(response => response.json())
+    .then(events => this.setState({ events, loading: false }))
+    .catch(error => console.log(error));
   }
   getSaved() {
-    fetch('http://localhost:3000/api/events/saved',{
+    fetch('http://localhost:3000/api/events/saved', {
       method: 'POST',
-      headers: { "Content-Type" : "application/json" },
-      body: JSON.stringify({userId: this.props.user._id})
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ userId: this.props.user.facebookId }),
     })
     .then(response => {
       return response.json();
@@ -85,24 +71,18 @@ export default class Feed extends Component {
     });
   }
   getBundle() {
-    fetch('http://localhost:3000/api/events/bundle',{
+    fetch('http://localhost:3000/api/events/bundle', {
       method: 'POST',
-      headers: { "Content-Type" : "application/json" },
-      body: JSON.stringify({userId: this.props.user._id, location: this.props.mongoLocation})
+      headers: { 'Content-Type' : 'application/json' },
+      body: JSON.stringify({userId: this.props.user.facebookId, location: this.props.mongoLocation})
     })
-    .then(response => {
-      return response.json();
-    })
-    .then( events => {
-      this.setState({events: events, loading: false});
-    })
-    .catch( error => {
-      console.log(error);
-    });
+    .then(response => response.json())
+    .then(events => this.setState({ events, loading: false }))
+    .catch(error => console.log(error));
   }
   getModal() {
     if (this.state.eventModal) {
-      return <EventModal close={this.closeEvent.bind(this)} user={this.props.user} visibility={this.state.eventModal} event={this.state.eventId}/>
+      return <EventModal close={this.closeEvent.bind(this)} user={this.props.user} visibility={this.state.eventModal} event={this.state.eventId} />
     } else {
       return (<View></View>)
     }
@@ -116,35 +96,35 @@ export default class Feed extends Component {
             <Spinner />
           </View>
         </OurDrawer>
-        )
+      )
     }
     return (
       <OurDrawer user={this.props.user} topBarFilterVisible={false} topBarName={this.props.name} _navigate={_navigate.bind(this)}>
         <ScrollView>
-          {this.state.events.map( (event, index) => <EventCard key={index} openModal={this.openEvent.bind(this)} event={event} index={index}/>)}
+          {this.state.events.map((event, index) => <EventCard key={index} openModal={this.openEvent.bind(this)} event={event} index={index}/>)}
         </ScrollView>
         <NewEventFab onPress={
           () => {
             this.props.navigator.resetTo({
-              name: 'Map'
+              name: 'Map',
             });
           }
-        }/>
+        } />
         {this.getModal()}
-        <NewEventModal visibility={this.state.addEventModal}/>
+        <NewEventModal visibility={this.state.addEventModal} />
       </OurDrawer>
-    )
+    );
   }
 };
 
- const drawerStyles = {
+const drawerStyles = {
   drawer: {
   backgroundColor: 'red',
   shadowColor: '#000000',
   shadowOpacity: 0.8,
   shadowRadius: 3,
-  }
-}
+  },
+};
 
 const styles = StyleSheet.create({
   listElem: {
@@ -158,7 +138,7 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     width: 225,
     padding: 10,
-    paddingLeft: 80
+    paddingLeft: 80,
 
   },
   button: {
@@ -173,6 +153,6 @@ const styles = StyleSheet.create({
   spinner: {
     padding: 30,
     marginTop: 200,
-    alignItems: 'center'
-  }
+    alignItems: 'center',
+  },
 });
